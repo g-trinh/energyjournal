e783667cd71ae759d1d2d964fd4eb615207b68b3
Guillaume Trinh <guillaume@trinh.tech>
2026-02-11 00:16:23 +0100

    feat(frontend): add landing route and move timespending to /timespending

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,18 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
-import AuthPage from './pages/AuthPage.tsx'
 import ActivatePage from './pages/ActivatePage.tsx'
+import AuthPage from './pages/AuthPage.tsx'
+import LandingPage from './pages/LandingPage.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/timespending" element={<App />} />
         <Route path="/auth" element={<AuthPage />} />
         <Route path="/activate" element={<ActivatePage />} />
-        <Route path="/" element={<App />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
---
+++ front/src/pages/LandingPage.tsx
@@ -0,0 +1,5 @@
+import '../App.css'
+
+export default function LandingPage() {
+  return <main aria-label="Landing page" />
+}
